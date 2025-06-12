c0a2cf33154c696a36e29e396d847319c15b1b81
Akin-Genc0 <akingenc212@gmail.com>
2025-06-12 14:12:46 +0100

    first commit

---
+++ index4.js
@@ -0,0 +1,6 @@
+const bookAuthor = document.querySelector('#author');
+const bookTitle = document.querySelector('#title');
+const addBtn = document.querySelector('#addBtn');
+const result = document.querySelector('.result');
+
+
